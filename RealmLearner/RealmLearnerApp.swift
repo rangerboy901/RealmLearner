b280c673cfe522a50787270ba28503a7deece0f8
Joseph DeWeese <rangerboy901@icloud.com>
2022-09-11 11:43:25 -0500

    Initial Commit

---
+++ RealmLearner/RealmLearnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmLearnerApp.swift
+//  RealmLearner
+//
+//  Created by Joseph Wil;liam DeWeese on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RealmLearnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
